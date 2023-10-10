e7d4071b65ac04b6ab1fa98c237a4e9c78e441e8
Andrii_Hluzdovskyi <Andrii_Hluzdovskyi@EPUALVIW05BD>
2023-10-10 08:11:15 +0200

    Add project files.

---
+++ Input.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using GreetingLib;
+
+namespace GreetingWpfApp
+{
+    public partial class Input : Window
+    {
+        public Input()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowGreetingButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameTextBox.Text;
+            if (username.Length > 0)
+            {
+                string greeting = GreetingGenerator.GenerateGreeting(username); 
+
+                            Output outputWindow = new Output();
+                            outputWindow.ShowGreeting(greeting);
+                            Close();
+                            outputWindow.Show();
+            }
+            else
+            {
+                WarningTextBlock.Text = "Please enter your name!";
+            }
+            
+        }
+        private void UsernameTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                ShowGreetingButton_Click(sender, e);
+            }
+        }
+
+    }
+}
+
---
+++ Output.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace GreetingWpfApp
+{
+    public partial class Output : Window
+    {
+        public Output()
+        {
+            InitializeComponent();
+        }
+        public void ShowGreeting(string greeting)
+        {
+            OutputTextBlock.Text = greeting;
+        }
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        private void OutputWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                Submit_Click(sender, e);
+            }
+        }        
+    }
+}
